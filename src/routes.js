2630e4eddb029fca491bc2e7f8e8779fe3f7823e
Vially Israel <viallyisrael@hotmail.com>
2022-05-19 17:47:34 -0300

    autentication

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middleware/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
